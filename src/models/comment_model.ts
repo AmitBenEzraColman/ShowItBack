a67906f7da838fbbb8f6607dc1d588aef4c32eeb
hershyarin <yarin5805180@cs.colman.ac.il>
2025-03-07 15:45:18 +0200

    add user and auth flow

---
+++ src/models/comment_model.ts
@@ -0,0 +1,33 @@
+import mongoose, { ObjectId } from "mongoose";
+
+export interface IComment {
+    _id?: ObjectId;
+    description: string;
+    author: ObjectId;
+    reviewId: ObjectId;
+    timeStamp: Date;
+}
+
+const commentSchema = new mongoose.Schema<IComment>({
+    description: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    reviewId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+        required: true,
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+});
+
+export default mongoose.model<IComment>("Comment", commentSchema);
